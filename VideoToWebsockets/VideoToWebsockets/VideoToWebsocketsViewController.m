6d7b189eb3607bfb71cb7d9137369e955a46ceba
obviousjim <james@jamesgeorge.org>
2011-12-02 13:32:04 -0500

    added .a files
    Signed-off-by: obviousjim <james@jamesgeorge.org>

--- VideoToWebsockets/VideoToWebsockets/VideoToWebsocketsViewController.m
+++ VideoToWebsockets/VideoToWebsockets/VideoToWebsocketsViewController.m
@@ -29,8 +29,8 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
-    self.socketConnection = [[VTWSVideoSenderConnection alloc] init];
-    [self.socketConnection startMyWebSocket];  
+    //self.socketConnection = [[VTWSVideoSenderConnection alloc] init];
+    //[self.socketConnection startMyWebSocket];  
     
     [super viewDidLoad];
 }
